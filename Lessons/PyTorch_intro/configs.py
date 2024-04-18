53f7e21726dd1276aa7a3a08b1575abe719fb01a
gabribg88 <gabrielemaroni235@gmail.com>
2024-04-18 14:04:35 +0200

    Added PyTorch intro lesssons.

---
+++ Lessons/PyTorch_intro/configs.py
@@ -0,0 +1,39 @@
+####
+fig_width = 6
+fig_height = 6
+fig_size =  [fig_width,fig_height]
+fig_params = {'lines.linewidth': 1,
+              'axes.grid': True,
+              'axes.xmargin': 0,
+              'axes.ymargin': 0,
+              'grid.linestyle': '--',
+              'grid.linewidth': 0.5,
+              'axes.labelsize': 13,
+              'axes.titlesize': 14,
+              'font.size': 11,
+              'legend.fontsize': 12,
+              'xtick.labelsize': 12,
+              'ytick.labelsize': 12,
+              'figure.figsize': fig_size,
+              'savefig.dpi': 600,
+              'axes.linewidth': 1.0,
+              ##
+              'xtick.direction': 'in',
+              'xtick.major.size': 3,
+              'xtick.major.width': 0.5,
+              'xtick.minor.size': 1.5,
+              'xtick.minor.width': 0.5,
+              'xtick.minor.visible': True,
+              'xtick.top': True,
+              ##
+              'ytick.direction': 'in',
+              'ytick.major.size': 3,
+              'ytick.major.width': 0.5,
+              'ytick.minor.size': 1.5,
+              'ytick.minor.width': 0.5,
+              'ytick.minor.visible': True,
+              'ytick.right': True,
+              ##
+              'font.family': 'serif',
+              'mathtext.fontset': 'dejavuserif',
+              }
