34f1f0743cdae623fee09a41e088a40008caddb4
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-04-26 12:04:27 +0200

    Further improvements to the implementation. Generalized for variable sequence length.

--- Exercises/dynonet.py
+++ Exercises/dynonet.py
@@ -3,6 +3,7 @@ Keras implementation of the dynonet
 """
 import os
 
+import keras.saving
 import pandas as pd
 import numpy as np
 
@@ -18,32 +19,44 @@ class F16DsSeq():
   Dataset Constructor
   """
 
-  def __init__(self, pd_file, l):
+  def __init__(self, pd_file, l, ar_order, use_context=False):
     output_names = [f"Acceleration{idx + 1}" for idx in range(3)]
     self.pd_file = pd_file
     self.seq_len = l
 
-    n = int(np.floor(len(pd_file) / l))
+    if use_context:
+      n = int(np.floor((len(pd_file) - ar_order) / l))
+      adv_idx = np.arange(n * l).reshape([n, l]) + ar_order
+    else:
+      n = int(np.floor(len(pd_file) / l))
+      adv_idx = np.arange(n * l).reshape([n, l])
 
-    adv_idx = np.arange(n * l).reshape([n, l])
-
-    u = np.stack([pd_file["Force"].to_numpy()[adv_idx],
+    x = np.stack([pd_file["Force"].to_numpy()[adv_idx],
                   pd_file["Voltage"].to_numpy()[adv_idx]], axis=-1)
 
     y = np.stack([pd_file[variable].to_numpy()[adv_idx]
                   for variable in output_names], axis=-1)
 
-    self.u = jax.numpy.asarray(u)
+    if use_context:
+      x_0 = np.stack([pd_file[variable].to_numpy()[
+          adv_idx[:, :ar_order] - ar_order]
+          for variable in output_names], axis=-1)
+      self.u = [jax.numpy.asarray(x), jax.numpy.asarray(x_0)]
+    else:
+      self.u = [jax.numpy.asarray(x), jax.numpy.zeros_like(y)[:, :ar_order, :]]
     self.y = jax.numpy.asarray(y)
 
 
+@keras.saving.register_keras_serializable(package="dynonet")
 class MimoLinearDynamicalOperator(keras.layers.Layer):
   """
   Apply MIMO filter
   """
 
-  def __init__(self, out_channels: int = 1, n_b: int = 1, n_a: int = 0):
-    super(MimoLinearDynamicalOperator, self).__init__()
+  def __init__(self, out_channels: int = 1,
+               n_b: int = 1, n_a: int = 0,
+               **kwargs):
+    super(MimoLinearDynamicalOperator, self).__init__(**kwargs)
 
     self.out_channels = out_channels
     self.n_b = n_b
@@ -54,65 +67,95 @@ class MimoLinearDynamicalOperator(keras.layers.Layer):
         [self.n_a, self.out_channels],
         initializer="zeros")
     self.b_coeff = self.add_weight(
-        [self.n_b, input_shape[-1], self.out_channels])
+        [self.n_b, input_shape[0][-1], self.out_channels])
 
-  def _ar_pply(self, x0, ut):
+  def _ar_pply(self, carry, ut):
+    u0, x0 = carry
+
+    u = keras.ops.concatenate([u0[1:, ...], ut[None, ...]])
+
     xt = keras.ops.einsum("to,tbo->bo",
                           keras.ops.flip(self.a_coeff, axis=0),
-                          x0) + ut
-    return keras.ops.concatenate([x0[1:, ...], xt[None, ...]]), xt
+                          x0) +\
+        keras.ops.einsum("tio,tbi->bo",
+                         keras.ops.flip(self.b_coeff, axis=0),
+                         u)
 
-  def _mimo_apply(self, inputs):
-    filtered_input = keras.ops.conv(
-        keras.ops.pad(inputs, [[0, 0], [self.n_b - 1, 0], [0, 0]]),
-        self.b_coeff,
-        padding="valid",
-        data_format="channels_last")
+    x = keras.ops.concatenate([x0[1:, ...], xt[None, ...]])
 
-    padded_shape = [self.n_a,
-                    inputs.shape[0],
-                    self.out_channels]
+    return [u, x], xt
+
+  def call(self, inputs: keras.KerasTensor):
+    u, x0 = inputs
 
     _, filtered_output = jax.lax.scan(self._ar_pply,
-                                      keras.ops.zeros(padded_shape),
-                                      keras.ops.transpose(filtered_input,
-                                                          [1, 0, 2]))
+                                      [keras.ops.zeros([self.n_b,
+                                                        u.shape[0],
+                                                        u.shape[-1]]),
+                                       keras.ops.transpose(x0, [1, 0, 2])],
+                                      keras.ops.transpose(u, [1, 0, 2]),
+                                      unroll=3)
 
     return keras.ops.transpose(filtered_output, [1, 0, 2])
 
-  def call(self, inputs: keras.KerasTensor):
-    return self._mimo_apply(inputs)
 
-
+@keras.saving.register_keras_serializable(package="dynonet")
 class DynoNet(keras.Model):
   """
   Slides' DynoNet implemented as keras-jax model
   """
 
-  def __init__(self, hidden_size, output_size, n_a, n_b):
-    super(DynoNet, self).__init__()
+  def __init__(self, hidden_size, encoding_depth, output_size,
+               n_a, n_b,
+               **kwargs):
+    super(DynoNet, self).__init__(**kwargs)
     self.hidden_size = hidden_size
+    self.encoding_depth = encoding_depth
     self.output_size = output_size
     self.n_a = n_a
     self.n_b = n_b
 
-    self.g1 = MimoLinearDynamicalOperator(4, n_b, n_a)
+    self.g1 = MimoLinearDynamicalOperator(encoding_depth, n_b, n_a)
     self.f = keras.Sequential([
         keras.layers.Dense(hidden_size, activation="sigmoid"),
-        keras.layers.Dense(3)
+        keras.layers.Dense(encoding_depth)
     ])
     self.g2 = MimoLinearDynamicalOperator(output_size, n_b, n_a)
     self.g_lin = MimoLinearDynamicalOperator(output_size, n_b, n_a)
 
+  def get_config(self):
+    config = super().get_config()
+    config.update({
+        "hidden_size": self.hidden_size,
+        "encoding_depth": self.encoding_depth,
+        "output_size": self.output_size,
+        "n_a": self.n_a,
+        "n_b": self.n_b
+    })
+    return config
+
   def build(self, input_shape):
-    self.g1.build(input_shape)
-    self.g2.build(list(input_shape)[:-1] + [3])
-    self.g_lin.build(input_shape)
+    x = keras.Input([None, input_shape[0][-1]])
+    x0_hidden = keras.Input([self.n_a, self.encoding_depth])
+    x0 = keras.Input([self.n_a, self.output_size])
+
+    x1 = self.g1([x, x0_hidden])
+    xf = self.f(x1)
+    self.g2([xf, x0])
+    self.g_lin([x, x0])
 
   def call(self, inputs):
-    x = self.g1(inputs)
+    x, x0 = inputs
+
+    x0_shape = list(x0.shape)
+    x0_shape[-2] = self.n_a
+    x0_shape[-1] = self.encoding_depth
+    x0_aux = keras.ops.zeros(x0_shape)
+
+    x = self.g1([x, x0_aux])
+
     x = self.f(x)
-    x = self.g2(x)
+    x = self.g2([x, x0])
 
     y = self.g_lin(inputs)
 
@@ -120,9 +163,9 @@ class DynoNet(keras.Model):
 
 
 if __name__ == "__main__":
-  na = 3
-  nb = 25
-  test_model = DynoNet(16, 3, na, nb)
+  na = 25
+  nb = 100
+  test_model = DynoNet(32, 8, 3, na, nb)
 
   folder = os.path.join("..", "Datasets", "F16")
   f_train_ds = os.path.join(folder, "F16Data_SineSw_Level3.csv")
@@ -139,8 +182,8 @@ if __name__ == "__main__":
   dict_ds["train"] = (dict_ds["train"] - ds_mean) / ds_std
   dict_ds["test"] = (dict_ds["test"] - ds_mean) / ds_std
 
-  seq_len = 500  # Length of the training sub-sequences
-  batch_size = 4  # batch size for the data loader
+  seq_len = 1000  # Length of the training sub-sequences
+  batch_size = 8  # batch size for the data loader
 
   folder = os.path.join("..", "Datasets", "F16")
   f_train_ds = os.path.join(folder, "F16Data_SineSw_Level3.csv")
@@ -156,15 +199,28 @@ if __name__ == "__main__":
   dict_ds["test"] = (dict_ds["test"] - ds_mean) / ds_std
 
   # Create instance of the class F16DS_seq and plot shape of inputs and outputs
-  F16DS_train = F16DsSeq(pd_file=dict_ds["train"], l=seq_len)
-
-  print(F16DS_train.u.shape)
+  F16DS_train = F16DsSeq(pd_file=dict_ds["train"],
+                         l=seq_len, ar_order=na,
+                         use_context=True)
 
   test_model.compile("adam", "mse")
-  test_model.fit(F16DS_train.u, F16DS_train.y,
-                 batch_size=batch_size, epochs=100)
+  test_model.fit(F16DS_train.u,
+                 F16DS_train.y,
+                 batch_size=batch_size, epochs=32)
 
-  F16DS_test = F16DsSeq(pd_file=dict_ds["test"], l=seq_len)
+  print(f"Linear AR weights:\n{
+      np.mean(np.abs(np.array(test_model.g_lin.a_coeff)), axis=-1)}")
+  print(f"Non-Linear AR weights 1:\n{
+      np.mean(np.abs(np.array(test_model.g1.a_coeff)), axis=-1)}")
+  print(f"Non-Linear AR weights 2:\n{
+      np.mean(np.abs(np.array(test_model.g2.a_coeff)), axis=-1)}")
+
+  test_model.save("./dynonet.keras")
+  test_model = keras.saving.load_model("./dynonet.keras")
+
+  F16DS_test = F16DsSeq(pd_file=dict_ds["test"],
+                        l=seq_len, ar_order=na,
+                        use_context=True)
   y_true = np.array(F16DS_test.y)
   plot_idx = np.argmax(
       np.mean(y_true - np.mean(y_true, 1, keepdims=True), axis=(1, 2)))
