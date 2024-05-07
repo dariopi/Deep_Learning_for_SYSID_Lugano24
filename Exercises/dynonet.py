b59b43750b5e83d739907461165cc1c4c21e78eb
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-05-07 09:49:13 +0200

    Incorporated SS representation; proper Input modification required to avoid wasting samples.

--- Exercises/dynonet.py
+++ Exercises/dynonet.py
@@ -3,12 +3,15 @@ Keras implementation of the dynonet
 """
 import os
 
+from enum import IntEnum
+
 import pandas as pd
 import numpy as np
 
 import keras
-from keras import layers, ops
+from keras import layers, ops, constraints, initializers
 from keras import KerasTensor as Tensor
+from keras import Sequential, Model, Input
 
 import jax
 from jax import lax
@@ -51,15 +54,172 @@ class F16DsSeq():
 
 
 @keras.saving.register_keras_serializable(package="dynonet")
-class MimoLinearDynamicalOperator(layers.Layer):
+class SystemMode(IntEnum):
+  TF = 0
+  SS = 1
+
+  def get_config(self):
+    return {
+        "TF": self.TF,
+        "SS": self.SS
+    }
+
+
+@keras.saving.register_keras_serializable(package="dynonet")
+class Clip(constraints.Constraint):
+  """Clips the value between a minimum and a maximum"""
+
+  def __init__(self, min_value, max_value, rate: float = 1.):
+    self.min_value = min_value or -jax.numpy.inf
+    self.max_value = max_value or jax.numpy.inf
+    self.rate = rate
+
+  def __call__(self, w):
+    return (1. - self.rate) * w + self.rate * ops.clip(
+        w, self.min_value, self.max_value)
+
+
+@keras.saving.register_keras_serializable(package="dynonet")
+class LinearDynamicalSS(layers.Layer):
   """
-  Apply MIMO filter
+  Structured Linear Dynamical System in the TF representation
+  """
+
+  def __init__(self, out_channels: int = 1,
+               n_x: int = 0,
+               rmin: float = 0.0,
+               rmax: float = 1.0,
+               max_phase: float = jax.numpy.pi,
+               **kwargs):
+    super(LinearDynamicalSS, self).__init__(**kwargs)
+
+    self.out_channels = out_channels
+    self.n_x = n_x
+
+    def nu_init(shape, dtype=jax.numpy.float32):
+      aux = initializers.RandomUniform(0., 1.)(shape, dtype)
+      return ops.cast(
+          ops.log(
+              -0.5 * ops.log(aux * (rmax + rmin) * (rmax - rmin) + rmin**2)),
+          dtype)
+    self.nu_log = self.add_weight(
+        [n_x], initializer=nu_init,
+        constraint=Clip(
+            ops.log(-ops.log(rmax)),
+            ops.log(-ops.log(rmin)),
+        ))
+
+    def theta_init(shape, dtype=jax.numpy.float32):
+      aux = initializers.RandomUniform(0., 1.)(shape, dtype)
+      return ops.cast(ops.log(max_phase * aux), dtype)
+    self.theta_log = self.add_weight(
+        [n_x], initializer=theta_init,
+        constraint=Clip(
+            None,
+            ops.log(max_phase)
+        ))
+
+    def gamma_init(shape, dtype=jax.numpy.float32):
+      lambda_mag = ops.exp(-ops.exp(self.nu_log))
+      return ops.cast(
+          ops.log(ops.sqrt(ops.ones(shape) - ops.square(lambda_mag))),
+          dtype)
+    self.gamma_log = self.add_weight(
+        [n_x], initializer=gamma_init,
+        constraint=Clip(
+            ops.log(ops.sqrt(1. - rmax**2)),
+            ops.log(ops.sqrt(1. - rmin**2))
+        ))
+
+  def build(self, input_shape):
+    n_u = input_shape[0][-1]
+
+    self.b_re = self.add_weight(
+        [self.n_x, n_u],
+        initializer=initializers.RandomNormal(
+            stddev=1. / np.sqrt(2 * n_u)
+        ))
+    self.b_im = self.add_weight(
+        [self.n_x, n_u],
+        initializer=initializers.RandomNormal(
+            stddev=1. / np.sqrt(2 * n_u)
+        ))
+
+    self.c_re = self.add_weight(
+        [self.out_channels, self.n_x],
+        initializer=initializers.RandomNormal(
+            stddev=1. / np.sqrt(self.n_x)
+        ))
+    self.c_im = self.add_weight(
+        [self.out_channels, self.n_x],
+        initializer=initializers.RandomNormal(
+            stddev=1. / np.sqrt(self.n_x)
+        ))
+
+    self.d = self.add_weight(
+        [self.out_channels, n_u],
+        initializer=initializers.RandomNormal(
+            stddev=1. / np.sqrt(n_u)
+        ))
+
+  def _ss_params(self):
+    lambda_mag = ops.exp(-ops.exp(self.nu_log))
+    lambda_phase = ops.exp(self.theta_log)
+
+    lambda_re = lambda_mag * ops.cos(lambda_phase)
+    lambda_im = lambda_mag * ops.sin(lambda_phase)
+    lambda_cx = lambda_re + lambda_im * 1j
+
+    b_matrix = self.gamma_log[:, None] * \
+        (self.b_re.value + self.b_im.value * 1j)
+    c_matrix = self.c_re.value + self.c_im.value * 1j
+
+    return lambda_cx, b_matrix, c_matrix, self.d.value
+
+  def call(self, inputs):
+    u, y0 = inputs
+    a, b, c, d = self._ss_params()
+
+    d_u = ops.einsum("oi,bti->bto", d, u)
+    if self.n_x == 0:
+      return d_u
+
+    # Initial state estimation (Past input assumed to be 0)
+    c_a_pinv = jax.numpy.linalg.pinv(ops.matmul(c, ops.diag(a)))
+    x0 = ops.einsum("so,bo->bs", c_a_pinv,
+                    y0[:, -1, :] - ops.einsum("oi,bi->bo",
+                                              d + ops.matmul(c, b),
+                                              ops.zeros_like(u[:, 0, :])
+                                              ))
+
+    # Calculate the effect of input over states
+    b_u = ops.concatenate([
+        (ops.einsum("si,bi->bs", b, u[:, 0, :])
+         + ops.einsum("s,bs->bs", a, x0))[:, None, :],
+        ops.einsum("si,bti->bts", b, u[:, 1:, :])
+    ], axis=1)
+
+    def a_apply(x0, b_u):
+      return x0 * a[None, None, :] + b_u
+
+    x = lax.associative_scan(a_apply, b_u, axis=1)
+    y = ops.einsum("os,bts->bto", c, x) + d_u
+
+    self.add_loss(ops.mean(ops.square(ops.imag(y))))
+
+    return ops.real(y)
+
+
+@keras.saving.register_keras_serializable(package="dynonet")
+class LinearDynamicalTF(layers.Layer):
+  """
+  Structured Linear Dynamical System in the TF representation
   """
 
   def __init__(self, out_channels: int = 1,
                n_b: int = 0, n_a: int = 0,
                **kwargs):
-    super(MimoLinearDynamicalOperator, self).__init__(**kwargs)
+    super(LinearDynamicalTF, self).__init__(**kwargs)
 
     self.out_channels = out_channels
     self.n_b = n_b
@@ -81,7 +241,7 @@ class MimoLinearDynamicalOperator(layers.Layer):
   def call(self, inputs: Tensor):
     u, x0 = inputs
 
-    # Calculate effect of input over output
+    # Calculate the effect of input over output
     b_u = ops.conv(
         ops.pad(u, [[0, 0], [self.n_b, 0], [0, 0]]),
         self.b_coeff, padding="valid", data_format="channels_last")
@@ -119,13 +279,14 @@ class MimoLinearDynamicalOperator(layers.Layer):
 
 
 @keras.saving.register_keras_serializable(package="dynonet")
-class DynoNet(keras.Model):
+class DynoNet(Model):
   """
   Slides' DynoNet implemented as keras-jax model
   """
 
   def __init__(self, hidden_size, encoding_depth, output_size,
                n_a, n_b,
+               system_mode: SystemMode = SystemMode.TF,
                **kwargs):
     super(DynoNet, self).__init__(**kwargs)
     self.hidden_size = hidden_size
@@ -134,13 +295,20 @@ class DynoNet(keras.Model):
     self.n_a = n_a
     self.n_b = n_b
 
-    self.g1 = MimoLinearDynamicalOperator(encoding_depth, n_b, n_a)
-    self.f = keras.Sequential([
+    self.system_mode = system_mode
+
+    if system_mode == SystemMode.TF:
+      self.g1 = LinearDynamicalTF(encoding_depth, n_b, n_a)
+      self.g2 = LinearDynamicalTF(output_size, n_b, n_a)
+      self.g_lin = LinearDynamicalTF(output_size, n_b, n_a)
+    elif system_mode == SystemMode.SS:
+      self.g1 = LinearDynamicalSS(encoding_depth, n_a)
+      self.g2 = LinearDynamicalSS(output_size, n_a)
+      self.g_lin = LinearDynamicalSS(output_size, n_a)
+    self.f = Sequential([
         layers.Dense(hidden_size, activation="sigmoid"),
         layers.Dense(encoding_depth)
     ])
-    self.g2 = MimoLinearDynamicalOperator(output_size, n_b, n_a)
-    self.g_lin = MimoLinearDynamicalOperator(output_size, n_b, n_a)
 
   def get_config(self):
     config = super().get_config()
@@ -149,21 +317,22 @@ class DynoNet(keras.Model):
         "encoding_depth": self.encoding_depth,
         "output_size": self.output_size,
         "n_a": self.n_a,
-        "n_b": self.n_b
+        "n_b": self.n_b,
+        "system_mode": self.system_mode
     })
     return config
 
   def build(self, input_shape):
-    x = keras.Input([None, input_shape[0][-1]])
-    x0_hidden = keras.Input([self.n_a, self.encoding_depth])
-    x0 = keras.Input([self.n_a, self.output_size])
+    x = Input([None, input_shape[0][-1]])
+    x0_hidden = Input([self.n_a, self.encoding_depth])
+    x0 = Input([self.n_a, self.output_size])
 
     x1 = self.g1([x, x0_hidden])
     xf = self.f(x1)
     self.g2([xf, x0])
     self.g_lin([x, x0])
 
-  def call(self, inputs):
+  def call(self, inputs: tuple[Tensor, Tensor]):
     x, x0 = inputs
 
     x0_shape = list(x0.shape)
@@ -182,9 +351,10 @@ class DynoNet(keras.Model):
 
 
 if __name__ == "__main__":
-  na = 50
-  nb = 100
-  test_model = DynoNet(32, 8, 3, na, nb)
+  mode = SystemMode.SS
+  na = 20
+  nb = 0
+  test_model = DynoNet(32, 8, 3, na, nb, system_mode=mode)
 
   folder = os.path.join("..", "Datasets", "F16")
   f_train_ds = os.path.join(folder, "F16Data_SineSw_Level3.csv")
@@ -225,14 +395,22 @@ if __name__ == "__main__":
   test_model.compile("adam", "mse")
   test_model.fit(F16DS_train.u,
                  F16DS_train.y,
-                 batch_size=batch_size, epochs=32)
+                 batch_size=batch_size, epochs=64)
 
-  print(f"Linear AR weights:\n{
-      np.mean(np.abs(np.array(test_model.g_lin.a_coeff)), axis=-1)}")
-  print(f"Non-Linear AR weights 1:\n{
-      np.mean(np.abs(np.array(test_model.g1.a_coeff)), axis=-1)}")
-  print(f"Non-Linear AR weights 2:\n{
-      np.mean(np.abs(np.array(test_model.g2.a_coeff)), axis=-1)}")
+  if mode == SystemMode.TF:
+    print(f"Linear AR weights:\n{
+        np.mean(np.abs(np.array(test_model.g_lin.a_coeff)), axis=-1)}")
+    print(f"Non-Linear AR weights 1:\n{
+        np.mean(np.abs(np.array(test_model.g1.a_coeff)), axis=-1)}")
+    print(f"Non-Linear AR weights 2:\n{
+        np.mean(np.abs(np.array(test_model.g2.a_coeff)), axis=-1)}")
+  elif mode == SystemMode.SS:
+    print(f"Linear AR weights:\n{
+        np.array(test_model.g_lin._ss_params()[0])}")
+    print(f"Non-Linear AR weights 1:\n{
+        np.array(test_model.g1._ss_params()[0])}")
+    print(f"Non-Linear AR weights 2:\n{
+        np.array(test_model.g2._ss_params()[0])}")
 
   test_model.save("./dynonet.keras")
   test_model = keras.saving.load_model("./dynonet.keras")
